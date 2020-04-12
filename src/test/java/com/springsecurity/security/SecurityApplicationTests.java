ffdf3c1d9dba235646e5cfb36c29ce65c9df5ed3
krishna192168 <krishnamoorthy.bavanudoss@revature.com>
2020-04-12 12:08:46 +0530

    skull of spring security

---
+++ src/test/java/com/springsecurity/security/SecurityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springsecurity.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
